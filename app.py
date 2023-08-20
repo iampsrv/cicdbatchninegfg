ce0175422d4b96afab76d60d0922d57733c9e0c1
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2023-08-20 13:33:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def delete_item_from_db(key):
     
 @app.route('/')
 def hello():
-    return "Hello Everyone"
+    return "Hello Pranjal"
     
 @app.route('/items', methods=['GET'])
 def get_all_items():
