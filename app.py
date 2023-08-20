7f4795af19ee5a68f4fe1e6d86fa437ef8b7219c
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2023-08-20 15:36:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def delete_item_from_db(key):
     
 @app.route('/')
 def hello():
-    return "testing jenkins with gitscm polling 1111"
+    return "testing jenkins with gitscm polling"
     
 @app.route('/items', methods=['GET'])
 def get_all_items():
